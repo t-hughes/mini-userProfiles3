59bb2751a7b2622d5341fdb7c1be2bf7b477fd34
Talon Hughes <talon.hughes@gmail.com>
2016-09-29 18:30:46 -0600

    finished mini project

--- controller.js
+++ controller.js
@@ -1,9 +1,10 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
   $scope.getUsers = function() {
     mainService.getUsers().then(function(dataFromService) {
-      $scope.users = dataFromService.data.data;
+      // $scope.users = dataFromService.data.data;//Rather than filter the data out in our controller we can use $q to filter it out as we pull it through our service.
+      $scope.users = dataFromService;
     });
-  }
+  };
 
   $scope.getUsers();
 
--- service.js
+++ service.js
@@ -1,10 +1,20 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    var deferred = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+    }).then(function(response){
+      var parsedResponse = response.data.data;
+      for(var i = 0; i < parsedResponse.length; i++) {
+        parsedResponse[i].first_name = 'Ralf';
+        parsedResponse[i].last_name = 'Lauren';
+        parsedResponse[i].avatar = 'http://memesvault.com/wp-content/uploads/Happy-Birthday-Meme-Nicolas-Cage-18.jpg';
+      }
+      deferred.resolve(parsedResponse);
+    });
+    return deferred.promise;
+  };
 
 });
